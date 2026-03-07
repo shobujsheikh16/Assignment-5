b1b6b59c8c727f29d36f13992ba237c0d810b671
shobujsheikh16 <mdshobujsheikh5177@gmail.com>
2026-03-08 03:26:42 +0600

    loadData

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -6,3 +6,14 @@ function showLoading() {
             <span class="loading loading-spinner loading-md text-[#4c00ff]"></span>
         </div>`;
 }
+async function loadData() {
+    showLoading();
+    try {
+        const res = await fetch('https://phi-lab-server.vercel.app/api/v1/lab/issues');
+        const result = await res.json();
+        allIssues = result.data || result || []; 
+        render(allIssues);
+    } catch (error) {
+        document.getElementById('grid').innerHTML = `<p class="col-span-full text-center py-10 text-red-500">Failed to load data.</p>`;
+    }
+}
